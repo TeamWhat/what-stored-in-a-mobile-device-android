1d601567efb5ab3b16428dd358e19f07735acb82
Henrik Nygren <nygrenh@gmail.com>
2014-09-04 15:37:27 +0300

    Add integration testing

---
+++ app/src/main/java/fi/hiit/whatisstoredinamobiledevice/ui/activities/MainScreen.java
@@ -0,0 +1,36 @@
+package fi.hiit.whatisstoredinamobiledevice.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import fi.hiit.whatisstoredinamobiledevice.R;
+
+public class MainScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ features/my_first.feature
@@ -0,0 +1,4 @@
+Feature: Test feature
+
+  Scenario: I can see the main screen
+    Then I see "Hello world!"
