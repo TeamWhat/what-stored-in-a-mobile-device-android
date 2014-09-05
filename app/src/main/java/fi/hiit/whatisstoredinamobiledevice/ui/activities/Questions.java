b2732b7a8056b1d97a0ddc8392321ed453327b30
Juho Leinonen <messionyk@gmail.com>
2014-09-05 14:34:22 +0300

    Implement sliding layout for questions

--- app/src/main/java/fi/hiit/whatisstoredinamobiledevice/ui/activities/Questions.java
+++ app/src/main/java/fi/hiit/whatisstoredinamobiledevice/ui/activities/Questions.java
@@ -2,8 +2,13 @@ package fi.hiit.whatisstoredinamobiledevice.ui.activities;
 
 import android.app.Activity;
 import android.app.ActionBar;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,18 +17,22 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 import fi.hiit.whatisstoredinamobiledevice.R;
+import fi.hiit.whatisstoredinamobiledevice.ui.fragments.Question1;
+import fi.hiit.whatisstoredinamobiledevice.ui.fragments.utilities.QuestionsPagerAdapter;
 
-public class Questions extends Activity {
+public class Questions extends FragmentActivity {
+
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.questions, new PlaceholderFragment())
-                    .commit();
-        }
+
+        mPager = (ViewPager) findViewById(R.id.questions);
+        mPagerAdapter = new QuestionsPagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
     }
 
 
---
+++ app/src/main/java/fi/hiit/whatisstoredinamobiledevice/ui/fragments/utilities/QuestionsPagerAdapter.java
@@ -0,0 +1,31 @@
+package fi.hiit.whatisstoredinamobiledevice.ui.fragments.utilities;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import fi.hiit.whatisstoredinamobiledevice.ui.fragments.Question1;
+import fi.hiit.whatisstoredinamobiledevice.ui.fragments.Question2;
+
+public class QuestionsPagerAdapter extends FragmentStatePagerAdapter {
+
+    public QuestionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position) {
+            case 0:
+                return new Question1();
+            case 1:
+                return new Question2();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
