5e0b49d41990a84fea916938e1bbb2636b75e7f6
Juho P O Leinonen <leju@dat-2.cs.helsinki.fi>
2014-09-05 12:36:21 +0300

    Add questions activity

--- app/src/main/java/fi/hiit/whatisstoredinamobiledevice/ui/activities/Questions.java
+++ app/src/main/java/fi/hiit/whatisstoredinamobiledevice/ui/activities/Questions.java
@@ -1,27 +1,36 @@
 package fi.hiit.whatisstoredinamobiledevice.ui.activities;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.ActionBar;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
 
 import fi.hiit.whatisstoredinamobiledevice.R;
 
-public class MainScreen extends Activity {
+public class Questions extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
+        setContentView(R.layout.activity_questions);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.questions, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_screen, menu);
+        getMenuInflater().inflate(R.menu.questions, menu);
         return true;
     }
 
@@ -37,8 +46,19 @@ public class MainScreen extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void openQuestions(View view) {
-        Intent intent = new Intent(this, Questions.class);
-        startActivity(intent);
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_question1, container, false);
+            return rootView;
+        }
     }
 }
--- features/my_first.feature
+++ features/my_first.feature
@@ -1,4 +1,5 @@
 Feature: Test feature
 
   Scenario: I can see the main screen
+    When I press button "Input your info"
     Then I see "Hello world!"
