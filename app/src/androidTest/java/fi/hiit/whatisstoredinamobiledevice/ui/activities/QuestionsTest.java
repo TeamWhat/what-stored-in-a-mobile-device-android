e56cadb77d2873da034588e2bab78d7390b597fc
Juho Leinonen <messionyk@gmail.com>
2014-09-11 16:43:52 +0300

    Fix and clean up test

--- app/src/androidTest/java/fi/hiit/whatisstoredinamobiledevice/ui/activities/QuestionsTest.java
+++ app/src/androidTest/java/fi/hiit/whatisstoredinamobiledevice/ui/activities/QuestionsTest.java
@@ -1,25 +1,12 @@
 package fi.hiit.whatisstoredinamobiledevice.ui.activities;
 
-import android.app.Activity;
-import android.app.Application;
-import android.support.v4.app.Fragment;
-import android.app.Instrumentation;
-import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.ActivityUnitTestCase;
-import android.test.ApplicationTestCase;
-import android.test.InstrumentationTestCase;
+import android.test.TouchUtils;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.util.DisplayMetrics;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.KeyEvent;
-import android.view.View;
-import android.widget.Button;
-import android.test.TouchUtils;
-
-import fi.hiit.whatisstoredinamobiledevice.R;
-import fi.hiit.whatisstoredinamobiledevice.ui.fragments.Question1;
 
 
 public class QuestionsTest extends ActivityInstrumentationTestCase2<Questions> {
@@ -36,25 +23,11 @@ public class QuestionsTest extends ActivityInstrumentationTestCase2<Questions> {
         questions = getActivity();
     }
 
-
     @MediumTest
     public void testOnBackPressedReturnsToFirstFragmentWhenOnSecondFragment() {
         swipeToRight();
         pressBack();
         assertEquals(0, getActivity().getPager().getCurrentItem());
-    }
-
-    private void swipeToRight(){
-        DisplayMetrics dm = new DisplayMetrics();
-        getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-
-        TouchUtils.drag(this, width -30, 30, height/2, height/2, 5);
-    }
-
-    private void pressBack() {
-        this.sendKeys(KeyEvent.KEYCODE_BACK);
     }
 
     @MediumTest
@@ -67,8 +40,28 @@ public class QuestionsTest extends ActivityInstrumentationTestCase2<Questions> {
 
         pressBack();
 
-        Question1 question1 = new Question1();
+        // need to wait until backpress is finished
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            Log.wtf("Questions", "Too many energy drinks son, couldn't sleep");
+            e.printStackTrace();
+        }
+
         assertFalse(questions.hasWindowFocus());
+    }
+
+    public void pressBack() {
+        this.sendKeys(KeyEvent.KEYCODE_BACK);
+    }
+
+    public void swipeToRight(){
+        DisplayMetrics dm = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        TouchUtils.drag(this, width - 30, 30, height / 2, height / 2, 5);
     }
 
 }
