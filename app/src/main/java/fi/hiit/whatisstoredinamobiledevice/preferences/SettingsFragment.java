bb83a88b5f579a7d7a97346ef7298a7f67757b3b
Eemi Haukkala <eemi.haukkala@gmail.com>
2014-09-12 12:26:16 +0300

    settings menu skeleton set up

---
+++ app/src/main/java/fi/hiit/whatisstoredinamobiledevice/preferences/SettingsFragment.java
@@ -0,0 +1,18 @@
+package fi.hiit.whatisstoredinamobiledevice.preferences;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import fi.hiit.whatisstoredinamobiledevice.R;
+
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+}
