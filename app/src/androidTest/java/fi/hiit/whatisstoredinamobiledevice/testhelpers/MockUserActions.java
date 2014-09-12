dff8e00070f65c93208bbd7dac5945ba21218c8f
Juho Leinonen <messionyk@gmail.com>
2014-09-12 11:50:03 +0300

    Refactor test helper methods

---
+++ app/src/androidTest/java/fi/hiit/whatisstoredinamobiledevice/testhelpers/MockUserActions.java
@@ -0,0 +1,37 @@
+package fi.hiit.whatisstoredinamobiledevice.testhelpers;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.KeyEvent;
+
+public class MockUserActions {
+
+    public void pressBack(ActivityInstrumentationTestCase2 aitc2) {
+        aitc2.sendKeys(KeyEvent.KEYCODE_BACK);
+        waitUntilActionFinished();
+    }
+
+    public void swipeToRight(Activity activity, ActivityInstrumentationTestCase2 aitc2){
+        DisplayMetrics dm = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        TouchUtils.drag(aitc2, width - 30, 30, height / 2, height / 2, 5);
+    }
+
+    /**
+     * Waits for 500ms to let emulator finish the mock action
+     */
+    private void waitUntilActionFinished() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            Log.wtf("MockUserActions", "Thread sleep failed for some reason");
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/androidTest/java/fi/hiit/whatisstoredinamobiledevice/ui/activities/QuestionsTest.java
+++ app/src/androidTest/java/fi/hiit/whatisstoredinamobiledevice/ui/activities/QuestionsTest.java
@@ -2,15 +2,14 @@ package fi.hiit.whatisstoredinamobiledevice.ui.activities;
 
 import android.support.v4.view.ViewPager;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.TouchUtils;
 import android.test.suitebuilder.annotation.MediumTest;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.KeyEvent;
+
+import fi.hiit.whatisstoredinamobiledevice.testhelpers.MockUserActions;
 
 
 public class QuestionsTest extends ActivityInstrumentationTestCase2<Questions> {
     private Questions questions;
+    private MockUserActions mockUserActions;
 
     public QuestionsTest() {
         super(Questions.class);
@@ -21,12 +20,13 @@ public class QuestionsTest extends ActivityInstrumentationTestCase2<Questions> {
         super.setUp();
         setActivityInitialTouchMode(true);
         questions = getActivity();
+        mockUserActions = new MockUserActions();
     }
 
     @MediumTest
     public void testOnBackPressedReturnsToFirstFragmentWhenOnSecondFragment() {
-        swipeToRight();
-        pressBack();
+        mockUserActions.swipeToRight(questions, this);
+        mockUserActions.pressBack(this);
         assertEquals(0, getActivity().getPager().getCurrentItem());
     }
 
@@ -37,31 +37,8 @@ public class QuestionsTest extends ActivityInstrumentationTestCase2<Questions> {
 
     @MediumTest
     public void testOnBackPressedReturnsToMainScreenWhenOnFirstFragment() {
-
-        pressBack();
-
-        // need to wait until back press is finished
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            Log.wtf("Questions", "Too many energy drinks son, couldn't sleep");
-            e.printStackTrace();
-        }
-
+        mockUserActions.pressBack(this);
         assertFalse(questions.hasWindowFocus());
-    }
-
-    public void pressBack() {
-        this.sendKeys(KeyEvent.KEYCODE_BACK);
-    }
-
-    public void swipeToRight(){
-        DisplayMetrics dm = new DisplayMetrics();
-        getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-
-        TouchUtils.drag(this, width - 30, 30, height / 2, height / 2, 5);
     }
 
 }
