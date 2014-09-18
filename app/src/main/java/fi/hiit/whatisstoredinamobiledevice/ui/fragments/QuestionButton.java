336e9e9409e0f2d98d5a6322a6ca0f3b9e383e23
Henrik Nygren <nygrenh@gmail.com>
2014-09-18 13:20:51 +0300

    Make first time settings to use PrerenceFragment

---
+++ app/src/main/java/fi/hiit/whatisstoredinamobiledevice/ui/fragments/QuestionButton.java
@@ -0,0 +1,34 @@
+package fi.hiit.whatisstoredinamobiledevice.ui.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fi.hiit.whatisstoredinamobiledevice.R;
+
+public class QuestionButton extends Fragment{
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment Question2.
+     */
+    public static QuestionButton newInstance() {
+        QuestionButton fragment = new QuestionButton();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+    public QuestionButton() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.question_button, container, false);
+    }
+
+}
