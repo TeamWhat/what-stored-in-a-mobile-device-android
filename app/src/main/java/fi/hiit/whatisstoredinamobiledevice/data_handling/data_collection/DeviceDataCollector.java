19ab141f75ae596a46cd60994ed7bed25c08f1cc
Jussi Mertanen <juzax@hotmail.com>
2014-09-25 17:32:57 +0300

    Add device data collector

---
+++ app/src/main/java/fi/hiit/whatisstoredinamobiledevice/data_handling/data_collection/DeviceDataCollector.java
@@ -0,0 +1,20 @@
+package fi.hiit.whatisstoredinamobiledevice.data_handling.data_collection;
+
+import android.os.Build;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DeviceDataCollector implements DataCollector{
+
+    @Override
+    public Map getData() {
+        HashMap<String, String> data = new HashMap<String, String>();
+        data.put("Brand", Build.BRAND);
+        data.put("Device", Build.DEVICE);
+        data.put("Model", Build.MODEL);
+        data.put("Product", Build.PRODUCT);
+        data.put("Serial", Build.SERIAL);
+        return data;
+    }
+}
