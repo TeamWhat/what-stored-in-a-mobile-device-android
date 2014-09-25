8ac296ad46760051a84c3cc21a1d1987cafa0b43
Juho Leinonen <messionyk@gmail.com>
2014-09-25 16:11:11 +0300

    Add data handling skeleton

---
+++ app/src/main/java/fi/hiit/whatisstoredinamobiledevice/data_handling/DataHandlerIntentService.java
@@ -0,0 +1,16 @@
+package fi.hiit.whatisstoredinamobiledevice.data_handling;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class DataHandlerIntentService extends IntentService {
+
+    public DataHandlerIntentService() {
+        super("DataHandlerIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/fi/hiit/whatisstoredinamobiledevice/data_handling/data_collection/DataCollector.java
@@ -0,0 +1,7 @@
+package fi.hiit.whatisstoredinamobiledevice.data_handling.data_collection;
+
+import java.util.Map;
+
+public interface DataCollector {
+    public Map getData();
+}
