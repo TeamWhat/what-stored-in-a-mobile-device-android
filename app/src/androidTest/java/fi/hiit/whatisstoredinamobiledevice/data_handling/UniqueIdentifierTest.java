31fe25e15eca4662974fb813a2eb05ef5853965e
Henrik Nygren <nygrenh@gmail.com>
2014-10-17 15:07:33 +0300

    Implement unique identifier

---
+++ app/src/androidTest/java/fi/hiit/whatisstoredinamobiledevice/data_handling/UniqueIdentifierTest.java
@@ -0,0 +1,34 @@
+package fi.hiit.whatisstoredinamobiledevice.data_handling;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.test.InstrumentationTestCase;
+import android.test.mock.MockContext;
+
+import fi.hiit.whatisstoredinamobiledevice.testhelpers.TestSetup;
+
+import static org.mockito.Mockito.*;
+
+public class UniqueIdentifierTest extends InstrumentationTestCase{
+    private UniqueIdentifier identifier;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        TestSetup.setupMockito(this);
+        Context mockContext = mock(MockContext.class);
+        WifiManager wm = mock(WifiManager.class);
+        when(mockContext.getSystemService(Context.WIFI_SERVICE)).thenReturn(wm);
+        WifiInfo info = mock(WifiInfo.class);
+        when(info.getMacAddress())
+                .thenReturn("1")
+                .thenReturn("2");
+        when(wm.getConnectionInfo()).thenReturn(info);
+        identifier = new UniqueIdentifier(mockContext);
+    }
+
+    public void testTesetTetsTestsTests() {
+        assertFalse(identifier.identifier().equals(identifier.identifier()));
+    }
+}
