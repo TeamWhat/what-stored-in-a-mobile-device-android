3def1aeefe5de0401b71b08f825268fbf49796ae
Eemi Haukkala <eemi.haukkala@gmail.com>
2014-10-30 15:12:15 +0200

    Finished POST test method

--- app/src/main/java/fi/hiit/whatisstoredinamobiledevice/network/HttpPostHandler.java
+++ app/src/main/java/fi/hiit/whatisstoredinamobiledevice/network/HttpPostHandler.java
@@ -7,58 +7,79 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class HttpPostHandler {
 
+    private final String mStringDemoUrl;
+    private final String mJSONDemoUrl;
     private Context mContext;
 
 
     public HttpPostHandler(Context mContext) {
+        this.mStringDemoUrl = "http://pickingdigitalpockets.herokuapp.com/demos";
+        this.mJSONDemoUrl = "http://pickingdigitalpockets.herokuapp.com/demos";
         this.mContext = mContext;
     }
 
-    // Source: http://www.itworld.com/article/2702452/development/how-to-send-a-post-request-with-google-volley-on-android.html
-    public boolean postTestMessage(final String postMessage) {
 
-        // Instantiate the RequestQueue.
+    // http://www.itworld.com/article/2702452/development/how-to-send-a-post-request-with-google-volley-on-android.html
+    public boolean postTestMessage(final String postMessage) {
         RequestQueue queue = Volley.newRequestQueue(mContext);
 
-        // todo: Change to server post url
-        String url ="http://www.google.com";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Do something to mark that POST was completed
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // Do something to tell that there was an error with the POST
-                    }
-                }
-        )
-
+        StringRequest sr = new StringRequest(Request.Method.POST, mStringDemoUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.print(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.print(error);
+            }
+        })
 
         {
             @Override
             protected Map<String,String> getParams(){
                 Map<String,String> params = new HashMap<String, String>();
-                params.put("message",postMessage);
+                params.put("demo[message]", postMessage);
                 return params;
             }
         };
 
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+
+        queue.add(sr);
+        return true;
+    }
+
+    public boolean postTestJSON (final JSONObject jsonToSend) {
+        RequestQueue queue = Volley.newRequestQueue(mContext);
+
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, mStringDemoUrl, jsonToSend,
+
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        System.out.print(jsonObject);
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        System.out.print(volleyError);
+                    }
+                });
+
+        queue.add(jsonRequest);
         return true;
     }
 
