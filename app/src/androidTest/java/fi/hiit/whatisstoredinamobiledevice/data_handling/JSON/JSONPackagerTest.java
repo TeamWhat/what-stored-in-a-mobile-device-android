71bf2e0bdc054a94a097a9c20e053186b3d47677
Jussi Mertanen <juzax@hotmail.com>
2014-10-30 16:04:25 +0200

    Add tests for JSONPackager

---
+++ app/src/androidTest/java/fi/hiit/whatisstoredinamobiledevice/data_handling/JSON/JSONPackagerTest.java
@@ -0,0 +1,47 @@
+package fi.hiit.whatisstoredinamobiledevice.data_handling.JSON;
+
+import android.test.InstrumentationTestCase;
+
+import org.json.JSONObject;
+import java.util.HashMap;
+
+import fi.hiit.whatisstoredinamobiledevice.data_handling.DataHandler;
+import fi.hiit.whatisstoredinamobiledevice.data_handling.database_utilities.DeviceDataOpenHelper;
+import fi.hiit.whatisstoredinamobiledevice.data_handling.database_utilities.SQLiteDatabaseAccessor;
+import fi.hiit.whatisstoredinamobiledevice.testhelpers.TestSetup;
+
+public class JSONPackagerTest extends InstrumentationTestCase {
+    private HashMap<String, HashMap<String, String>> testMap;
+    private JSONPackager jsonPackager;
+    private JSONObject joe;
+
+    protected void setUp() {
+        TestSetup.setupMockito(this);
+        jsonPackager = new JSONPackager(getInstrumentation().getTargetContext());
+        testMap = new HashMap<String, HashMap<String, String>>();
+        HashMap<String, String> innerTestMap = new HashMap<String, String>();
+        innerTestMap.put("1", "2");
+        innerTestMap.put("3", "4");
+        testMap.put("0", innerTestMap);
+        joe = jsonPackager.createJsonObjectFromHashMap(testMap);
+        new DataHandler(getInstrumentation().getTargetContext(), new SQLiteDatabaseAccessor(new DeviceDataOpenHelper(getInstrumentation().getTargetContext()))).collectAllData();
+    }
+
+    public void testJSONObjectFromHashMapNotNull() {
+        assertTrue(joe != null);
+    }
+
+    public void testJSONObjectFromHashMapHasCorrectData() {
+        String expectedJson = "{\"0\":{\"3\":\"4\",\"1\":\"2\"}}";
+        assertTrue(joe.toString().equals(expectedJson.toString()));
+    }
+
+    public void testJSONObjectFromStoredData() {
+        // todo:Change this when we add tables
+        JSONObject jobj = jsonPackager.createJsonObjectFromStoredData();
+        assertTrue(jobj.length() == 3);
+        assertTrue(jobj.has("uid"));
+        assertTrue(jobj.has("image_info"));
+        assertTrue(jobj.has("device_info"));
+    }
+}
